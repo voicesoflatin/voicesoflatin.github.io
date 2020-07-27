f5a2fc35557b5564ace180babbe36c49b5a58276
voicesoflatin <68863452+voicesoflatin@users.noreply.github.com>
2020-07-27 19:21:51 +0200

    Create playaudio.js

---
+++ playaudio.js
@@ -0,0 +1,4 @@
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+<img class="cl" src="photo/198.jpg" />
+</br> <audio class="cs">
+<source src="http://www.w3schools.com/html/horse.mp3" type="audio/mpeg"> </audio>
