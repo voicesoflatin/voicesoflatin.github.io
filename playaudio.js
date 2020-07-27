d9bd41c6d78195e8ead69e40cf546ca2d3f57dd9
voicesoflatin <68863452+voicesoflatin@users.noreply.github.com>
2020-07-27 19:30:00 +0200

    Update playaudio.js

--- playaudio.js
+++ playaudio.js
@@ -1,3 +1,3 @@
-<a onclick="document.getElementById('Rce_Ib_Cat_Ctl_Gra_Granollers_Dl_839_taste_gustum.ogg').play();">
+<button onclick="document.getElementById('Rce_Ib_Cat_Ctl_Gra_Granollers_Dl_839_taste_gustum.ogg').play();">
 <img src="Screenshot_20200727-175847_Discord.jpg">
-</a>
+</button>
